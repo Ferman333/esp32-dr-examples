be4f44ede580f9fbb8821b65361501bd75b125b6
Ferman <32601316+Ferman333@users.noreply.github.com>
2025-10-01 00:21:07 -0600

    Revert "Revert "Added credential files (again)""
    This reverts commit 446a2b3fe2575a31301eac5fbc6f9d53eb3ae051.

---
+++ web_camera01/credentials.h
@@ -0,0 +1,8 @@
+#ifndef credentials_h
+#define credentials_h
+
+// Replace with your network credentials
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+
+#endif
