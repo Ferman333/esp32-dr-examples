0f0bd89d8c9deb4b60ff889f7b6b2c228b4f2926
Ferman <32601316+Ferman333@users.noreply.github.com>
2025-10-01 00:15:17 -0600

    Removed credentials files from index

--- oled02/credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef credentials_h
-#define credentials_h
-
-// Create custom network credentials for your AP device
-const char* ssid = "custom_ssid_for_wifi_AP";
-const char* password = "custom_password_for_wifi_AP";
-
-#endif
--- web_camera01/credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef credentials_h
-#define credentials_h
-
-// Replace with your network credentials
-const char* ssid = "your_wifi_ssid";
-const char* password = "your_wifi_password";
-
-#endif
