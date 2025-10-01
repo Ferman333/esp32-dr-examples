9ac8e11a3deb4f314f355c2414af00c7ec23c380
Ferman <32601316+Ferman333@users.noreply.github.com>
2025-10-01 00:18:12 -0600

    Added credential files (again)

---
+++ wifi02_esp32/credentials.h
@@ -0,0 +1,8 @@
+#ifndef credentials_h
+#define credentials_h
+
+// Create custom network credentials for your AP device
+const char* ssid = "custom_ssid_for_wifi_AP";
+const char* password = "custom_password_for_wifi_AP";
+
+#endif
